e0b9ca652da902d9a3a26819ecae143c4ab5e096
@sieg.engel <114477205+wolfsengel@users.noreply.github.com>
2022-10-24 17:24:43 +0200

    Create rockstarGames.java

---
+++ rockstarGames.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.Math;
+
+public class rockstarGames {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        calcularEntrada();
+    }
+    
+    public static void calcularEntrada() {
+        int idade;
+        double prezo;
+        
+        System.out.println("indica edad  cliente");
+        idade = input.nextInt();
+        if (idade < 0) {
+            System.out.println("Idade non valida");
+        } else {
+            if (idade < 4) {
+                prezo = 0;
+            } else {
+                if (idade < 18) {
+                    prezo = 5;
+                } else {
+                    prezo = 10;
+                }
+            }
+            System.out.println("El precio es: " + prezo);
+        }
+    }
+}
